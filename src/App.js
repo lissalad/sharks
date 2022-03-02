f0d60d9b21ae0a4759882f189a1462ab03ddf309
lissalad <elissalaymon@gmail.com>
2022-03-02 15:42:45 -0800

    useless variables eradicated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Header from "./components/Header";
-import Shark from './components/Shark';
 import { Outlet } from 'react-router-dom'
-import Gallery from './components/Gallery';
 
 function App() {
   return (
