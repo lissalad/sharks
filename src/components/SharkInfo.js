34c9893472852e562eed8005bd566140a407adec
lissalad <elissalaymon@gmail.com>
2022-11-21 11:47:32 -0800

    better responsive info page, rise in improved

--- src/components/SharkInfo.js
+++ src/components/SharkInfo.js
@@ -8,14 +8,16 @@ function SharkInfo(props) {
   const info = "font-bold";
 
   return (
-    <div className="flex flex-col mt-28 m-2 md:mx-9 md:my-12 md:flex-row md:justify-center md:space-x-5">
-      <img className="md:w-[500px]" src={`/images/${image}`} alt={name} />
-      <div className="flex flex-col">
-        <div className="flex flex-col mx-4 my-2 mb-3 md:items-center md:mb-6">
+    <div className="shark-info">
+      {/* image */}
+      <img className="shark-info-image" src={`/images/${image}`} alt={name} />
+      {/* text info */}
+      <div className="text-info space-y-9 mx-9">
+        <div className="flex flex-col my-2 mb-3 space-y-2">
           <h1 className="text-3xl md:text-5xl">{name}</h1>
-          <h2 className="text-md italic md:my-2 md:mr-9">{scientific_name}</h2>
+          <h2 className="text-md italic">{scientific_name}</h2>
         </div>
-        <div className="my-3 mx-6 space-y-4 md:ml-9">
+        <div className="my-3 space-y-4 text-lg">
           <p>
             <span className={info}>Size:</span> {size}
           </p>
